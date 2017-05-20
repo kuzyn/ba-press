588051ad0d1ef967859dad43ba57d464b9c04e04
kuzyn <hello@samuelcousin.com>
2017-05-20 09:55:35 -0400

    size triggers done

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,27 @@
 Zepto(function($) {
   
-  console.log('zepto');
-  $('.trigger-size').on('mouseenter', function(e) {
-    var type = e.srcElement.innerText;
-    $('.main-image-overlay').css('opacity', 1);
-  });
+  // need to instanciate the clip-path this way else first transition jag
+  $('.wrapper-main-image').css('clip-path', 'inset(0 0 0 0)');
 
+  // when we over the size triggers
+  $('.trigger-size').on('mouseenter', function(e) {
+    $('.main-image-overlay').css('opacity', 1);
+
+    if (e.srcElement.innerText === 'letter') {
+      $('.wrapper-main-image').css('clip-path', 'inset(0 0 0 260px)');
+      $('.main-image-overlay-width').addClass('overlay-width-letter'); 
+      $('.main-image-overlay-height').addClass('overlay-height-letter'); 
+    } else {
+      $('.main-image-overlay-width').addClass('overlay-width-ledger'); 
+      $('.main-image-overlay-height').addClass('overlay-height-ledger'); 
+    }
+  });
+  
+  // when we leave the size triggers (reset)
   $('.trigger-size').on('mouseleave', function(e) {
-    var type = e.srcElement.innerText;
     $('.main-image-overlay').css('opacity', 0);
+    $('.wrapper-main-image').css('clip-path', 'inset(0 0 0 0)');
+    $('.main-image-overlay').removeClass('overlay-width-ledger overlay-height-ledger overlay-width-letter overlay-height-letter'); 
   });
 
   $('.trigger-color').on('mouseenter', function(e) {
